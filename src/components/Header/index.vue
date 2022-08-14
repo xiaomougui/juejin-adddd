e2e84d44243af2bdcb051a2cb581121286ced1c1
zhouyaojia <103298497+zhouyaojia@users.noreply.github.com>
2022-08-14 12:21:25 +0800

    css样式修改

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -6,28 +6,22 @@
     @select="handleSelect"
   >
     <el-menu-item>
-      <!-- <a href="#" style="width: 107px; height: 22px"> -->
-      <!-- <img
-          src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/e08da34488b114bd4c665ba2fa520a31.svg"
-          class="logo"
-        /> -->
       <el-image
         style="width: 107px; height: 22px"
         src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/e08da34488b114bd4c665ba2fa520a31.svg"
         fit="contain"
       ></el-image>
-      <!-- </a> -->
     </el-menu-item>
-    <el-menu-item index="1">首页</el-menu-item>
-    <el-menu-item index="2">沸点</el-menu-item>
-    <el-menu-item index="3">课程</el-menu-item>
-    <el-menu-item index="4">直播</el-menu-item>
-    <el-menu-item index="5">活动</el-menu-item>
-    <el-menu-item index="6">商城</el-menu-item>
-    <el-menu-item index="7">APP</el-menu-item>
-    <el-menu-item index="8">插件</el-menu-item>
+    <el-menu-item index="1" id="shouye">首页</el-menu-item>
+    <el-menu-item index="2" id="wenzi">沸点</el-menu-item>
+    <el-menu-item index="3" id="wenzi">课程</el-menu-item>
+    <el-menu-item index="4" id="wenzi">直播</el-menu-item>
+    <el-menu-item index="5" id="wenzi">活动</el-menu-item>
+    <el-menu-item index="6" id="wenzi">商城</el-menu-item>
+    <el-menu-item index="7" id="wenzi">APP</el-menu-item>
+    <el-menu-item index="8" id="wenzi">插件</el-menu-item>
     <el-menu-item>
-      <div>
+      <div> 
         <el-input
           placeholder="探索稀土掘金"
           v-model="input3"
@@ -104,7 +98,22 @@ export default {
 .el-icon-arrow-down {
   font-size: 12px;
 }
-/* .logo {
-  width: 107px !important;
-} */
+
+#shouye{
+  color: #409EFF;
+  border-bottom-color: transparent;
+}
+#shouye:hover{
+  color: #252933;
+  border-bottom: 2px solid #409EFF!important;
+}
+#wenzi {
+  font-size: 13.92px;
+  color: #71777c;
+}
+
+#wenzi:hover {
+  color: #252933;
+  border-bottom: 2px solid #409EFF!important;
+}
 </style>
