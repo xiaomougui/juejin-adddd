a4f7179b7d6fdc1919104a43fe1e3c4bbe69f3e5
侯瑞宁 <94113626+xiaomougui@users.noreply.github.com>
2022-08-15 21:02:34 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <Nav />
     <Article />
   </div>
 </template>
@@ -21,14 +20,11 @@ export default {
 </script>
 
 <style>
-
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 60px;
-  
 }
 </style>
