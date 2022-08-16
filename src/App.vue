17e6ee63b49a36ff7d4b950500434f12b4da4600
侯瑞宁 <94113626+xiaomougui@users.noreply.github.com>
2022-08-16 20:07:08 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <div id="app">
-    <md></md>
+    <Header />
+    <Nav />
+    <Home />
   </div>
 </template>
 
 <script>
 import md from "./components/test/md.vue";
-import Nav from "./components/Nav";
+import Nav from "./components/Nav/index.vue";
+import Header from "./components/Header/index.vue";
 import Article from "./components/Article.vue";
+import Home from "./views/home/Home.vue";
 
 export default {
   name: "App",
   components: {
     md,
     Nav,
+    Header,
     Article,
+    Home,
   },
 };
 </script>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,58 +1,50 @@
 <template>
-    <div class="home">
-        <h1>这是主页</h1>
-        <button @click="test">click me</button>
-        <Passages :passages="passages"></Passages>
-        
-    </div>
+  <div class="home">
+    <h1>这是主页</h1>
+    <button @click="test">click me</button>
+    <Passages :passages="passages"></Passages>
+  </div>
 </template>
 
 <script>
-import Passages from "./childrenComps/Passages.vue"
+import Passages from "./childrenComps/Passages.vue";
 
-import { getHomeData} from "../../network/home";
+import { getHomeData } from "../../network/home";
 
-export default{
-    data(){
-        return{
-            passages:[],
-            obj:[]
-        }
+export default {
+  data() {
+    return {
+      passages: [],
+      obj: [],
+    };
+  },
+
+  components: {
+    Passages,
+  },
+
+  methods: {
+    getHomeData() {
+      getHomeData().then((res) => {
+        console.log(res);
+        this.passages = res;
+        this.obj = JSON.parse(res);
+        console.log(this.obj);
+      });
     },
 
-    components:{
-    Passages
-},
-
-    methods:{
-        getHomeData() {
-            getHomeData().then((res) => {
-                console.log(res)
-                this.passages = res
-                this.obj = JSON.parse(res)
-                console.log(this.obj)
-            });
-        },
-
-        test(){
-            console.log(this.passages)
-            console.log(this.passages[0])
-            console.log(this.passages[0].category)
-        }
+    test() {
+      console.log(this.passages);
+      console.log(this.passages[0]);
+      console.log(this.passages[0].category);
     },
+  },
 
-    created(){
-        this.getHomeData()
-        
-    }
-
-    
-
-
-    
-}
+  created() {
+    this.getHomeData();
+  },
+};
 </script>
 
 <style>
-
 </style>
